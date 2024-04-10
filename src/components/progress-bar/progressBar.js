a2f79e32f9226ee4b5415c9892a7a47aac13810c
Raniel <raniel.paula@fatec.sp.gov.br>
2024-04-10 20:12:04 -0300

    Finalizado Componente de Barra de Progresso

--- src/components/progress-bar/progressBar.js
+++ src/components/progress-bar/progressBar.js
@@ -1,47 +1,53 @@
 import SidebarMenu from "../../components/sidebar/sidebar";
 import Navbar from "../../components/navbar/navbar";
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { ProgressBar } from 'primereact/progressbar';
 import { Toast } from 'primereact/toast';
+import { Button } from 'primereact/button';
+
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";                                //icons      
 import "./progressBar.css";
+
 const ProgressoBarra = () => {
+    const [loading1, setLoading1] = useState(false);
     const [value1, setValue1] = useState(0);
     const toast = useRef(null);
     const interval = useRef(null);
 
-    useEffect(() => {
-        let val = value1;
+    const onLoadingClick1 = () => {
+        setLoading1(true);
+        setValue1(0);
+
         interval.current = setInterval(() => {
-            val += Math.floor(Math.random() * 10) + 1;
-            if (val >= 100) {
-                val = 100;
-                toast.current.show({ severity: 'info', summary: 'Successo', detail: 'Processamento Concluído' });
-                clearInterval(interval.current);
-            }
+            setValue1(prevValue => {
+                const newValue = prevValue + Math.floor(Math.random() * 10) + 1;
+                if (newValue >= 100) {
+                    clearInterval(interval.current);
+                    setLoading1(false);
+                    setValue1(100);
+                    toast.current.show({ severity: 'info', summary: 'Sucesso', detail: 'Processamento concluído' });
+                }
+                return newValue;
+            });
+        }, 1000);
+    };
 
-            setValue1(val);
-        }, 2000);
-
-        return () => {
-            if (interval.current) {
-                clearInterval(interval.current);
-                interval.current = null;
-            }
-        }
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
     return (
         <>
-            <SidebarMenu></SidebarMenu>
-            <Navbar title={"ProgressBar"}></Navbar>
-            <Toast ref={toast}></Toast>
+            <SidebarMenu />
+            <Navbar title={"ProgressBar"} />
+            <Toast ref={toast} />
             <div className="card">
-                <span className="title-process">Progresso</span>
-                <ProgressBar value={value1}></ProgressBar>
+                <div className="title-process">
+                    <span >Progresso do Processamento</span>
+                </div>
+                
+                
+                <ProgressBar value={value1} />
+                <Button className="btnIniciar" label="Iniciar" loading={loading1} onClick={onLoadingClick1} />
             </div>
-
         </>
     );
 };

fba922ccf4adaeab20b5aefbf0d50b95f0e3a365
Raniel <raniel.paula@fatec.sp.gov.br>
2024-04-12 19:44:19 -0300

    Finalizado FrontEnd do Login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,11 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import LogoLogin from "../../img/logo_login.png"
+import "./Login.css"
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
 
     return (
         <>
-            <Link style={{textAlign: 'center', fontSize: '60px'}} to="/dashboard">Login</Link>
+            <div className="container">
+                <div className="card-login">
+                    <div className="card-logo">
+                        <img className="logo-login" src={LogoLogin}></img>
+                        <div className="logo-text"> 
+                            <p>Esta é uma área restrita para usuários cadastrados.</p>
+                            <p>Informe seu usuário e senha para obter acesso ao sistema</p>
+                        </div>                        
+                    </div>
+                    <div className="card-form">
+                        <div className="inner-card">
+                            <h3 style={{color: '#292D32', marginTop: '20px'}}>Login</h3>
+                            <InputText className='espacamento-input1' value={email} placeholder='Email'  onChange={(e) => setEmail(e.target.value)} />
+                            <InputText className='espacamento-input2' type="password" value={senha} placeholder='Senha' onChange={(e) => setSenha(e.target.value)} />
+                            <Button onClick={() => window.location.href = '/dashboard'} className='btn-login' label="Entrar" aria-label="Submit"/>                                                   
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
